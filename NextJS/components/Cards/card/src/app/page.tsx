4ef39d045c47c5d070b27e1732efe2ccedccfb98
Anil Kumar K K <akumarkk@outlook.com>
2024-10-22 17:43:31 -0700

    card

---
+++ NextJS/components/Cards/card/src/app/page.tsx
@@ -0,0 +1,24 @@
+// import Image from "next/image";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react"
+
+export default function Home() {
+  return (
+    
+    <Card>
+      <CardHeader>
+      
+        Welcome to wlenz
+      </CardHeader>
+      <CardBody>
+      wlenz observability tool
+      
+      </CardBody>
+      <CardFooter>
+      wlenz observability tool  - contact us
+      
+      </CardFooter>
+      
+    </Card>
+    
+  );
+}
