1cb1595c9b1f5acc85fdd46d72e9e5143587013d
Anil Kumar K K <akumarkk@outlook.com>
2024-10-22 22:52:54 -0700

    formcontrol - materialui

--- NextJS/components/Cards/card/src/app/page.tsx
+++ NextJS/components/Cards/card/src/app/page.tsx
@@ -1,18 +1,57 @@
 // import Image from "next/image";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react"
+'use client';
+import {Divider, Card, CardHeader, CardBody, CardFooter, Link, Image, Checkbox,} from "@nextui-org/react"
+import {FormControlLabel} from '@mui/material'
+import styles from "./styles/app.css"
+// {}
 
+import {useState} from "react"
+// 'use client';
 export default function Home() {
+
+  // delRL, setDelRL
+  const [delRL, setDelRL] = useState(true)
+
+  const onChangeHandle = () => {
+    console.log(delRL)  
+    setDelRL(!delRL)
+  }
+
+  // <FormControlLabel label="Delete RL" control={<Checkbox className="checkbox" defaultChecked onChange={onChangeHandle} label ="Delete RL"\> }> </FormControlLabel>
+  // const [delete, setDelete] = useState(true)
   return (
     
-    <Card>
-      <CardHeader>
-      
+    <Card className={"max-w-[400px] card " + styles.card}>
+      <CardHeader className="flex gap-3">
+      <div className="flex flex-col">
         Welcome to wlenz
+      </div>
+      
+        
       </CardHeader>
+      <Divider></Divider>
       <CardBody>
-      wlenz observability tool
+        <Checkbox className="checkbox" css={{ marginRight: '8px !important' }} defaultChecked onChange={onChangeHandle} label ="Delete RL"> 
+        
+        {/*  */}
+        </Checkbox>
+        
+        
+        {/* <FormControlLabel 
+        label="Delete RL" 
+        control={<Checkbox className="checkbox" css={{ marginRight: '8px !important' }} defaultChecked onChange={onChangeHandle} label ="Delete RL"> }> 
+        </FormControlLabel> */}
+
+        <span style={{ margin: '8px' }}>Delete RL</span>
+{delRL && <div >
+        wlenz observability tool
+        </div> }
+
+        <FormControlLabel 
+        label="Delete RL" control={<Checkbox className="checkbox" defaultChecked onChange={onChangeHandle}></Checkbox> }> </FormControlLabel>
       
       </CardBody>
+      <Divider></Divider>
       <CardFooter>
       wlenz observability tool  - contact us
       
