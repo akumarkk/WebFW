c7fdc76ee26269c1cfd850374e1e8cfe69b2ccc6
Anil Kumar K K <akumarkk@outlook.com>
2024-10-27 23:13:58 -0700

    pizza comp

--- ReactJS/Pizza/pizza/src/index.js
+++ ReactJS/Pizza/pizza/src/index.js
@@ -4,8 +4,63 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 function App() {
-  return <h1>welcome to pizza store</h1>
+  return (
+
+  <div>
+    <h1>welcome to pizza store</h1>
+    <Pizza></Pizza>
+  </div>
+  )
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
