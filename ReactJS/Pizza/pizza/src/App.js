19d0803221033b673f5d297a30eb29fb02470f12
Anil Kumar K K <akumarkk@outlook.com>
2024-10-27 20:16:06 -0700

    pizza store

---
+++ ReactJS/Pizza/pizza/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      welcome to pizza store
+    </div>
+  );
+}
+
+export default App;
