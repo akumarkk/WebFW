ac8e27d93de80809e7c9698ce24dc66640c57980
Anil Kumar K K <akumarkk@outlook.com>
2024-11-30 14:58:37 -0800

    counter

---
+++ Vue/apps/counter/counter/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+const count = ref(0)
+function increment() { return count.value++;}
+</script>
+
+<template>
+  <div>
+    <button @click="increment"> you clicked at {{count}} times </button>
+  </div>
+</template>
+
+<style scoped>
+</style>
