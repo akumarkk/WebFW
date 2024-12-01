7efcb0f0b9d42f1619cd96178fcb6fb5506b8344
Anil Kumar K K <akumarkk@outlook.com>
2024-11-30 18:56:36 -0800

    Todo List

--- Vue/apps/counter/counter/src/App.vue
+++ Vue/apps/counter/counter/src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-const count = ref(0)
-function increment() { return count.value++;}
+import TodoList from './components/TodoList.vue'
+//const count = ref(0)
+
+//let count = 0;
+const count = ref(0) // causes re-render
+function increment() {
+  console.log(count.value);
+ return count.value++; }
 </script>
 
 <template>
   <div>
     <button @click="increment"> you clicked at {{count}} times </button>
+    <TodoList />
   </div>
 </template>
 
---
+++ Vue/apps/counter/counter/src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>
+        welcome to List app
+    </h2>
+</template>
+
+<script>
+    //export defualt {}
+
+</script>
+<style>
+body {
+    background-color: rgb(66, 66, 66);
+    color: blue;
+}
+
+</style>
