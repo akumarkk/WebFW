620aa786931c081636958624d46e77255bb0ea89
Anil Kumar K K <akumarkk@outlook.com>
2024-11-30 21:18:17 -0800

    todolist app

--- Vue/apps/counter/counter/src/components/TodoList.vue
+++ Vue/apps/counter/counter/src/components/TodoList.vue
@@ -7,6 +7,26 @@
 <script>
     //export defualt {}
 
+    export defualt {
+        data() {
+            return {
+                todos: [
+                    {todo: "Make angular course", completed :false},
+                    {todo: "Make swelte course", completed :false},
+                    {todo: "Make angular 2 course", completed :true}
+                
+                ]
+            }
+        },
+
+        methods: {
+            addToDo(todo) { this.todos.push(todo) },
+            editToDo(todo, etodo) {todo.todo = etodo},
+            toggleToDo(todo) {todo.completed = !todo.completed},
+            deleteToDo(todo) { this.todos = this.todo.filter(t => t.todo != todo)},
+        }
+    }
+
 </script>
 <style>
 body {
