005c7bd5d9b87ad41744287c93fce6d84dcd218b
Anil Kumar K K <akumarkk@outlook.com>
2024-11-30 23:20:32 -0800

    todo list

--- Vue/apps/counter/counter/src/components/TodoList.vue
+++ Vue/apps/counter/counter/src/components/TodoList.vue
@@ -1,37 +1,55 @@
 <template>
-    <h2>
-        welcome to List app
-    </h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <p class="display-3">To do List</p>
+      </div>
+    </div>
+    <div class="row">
+      <p>Add create form</p>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <ul class="list-group">
+          <li>item -1</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    //export defualt {}
+export default {
+  data() {
+    return {
+      todos: [
+        { todo: "Make angular course", completed: false },
+        { todo: "Make swelte course", completed: false },
+        { todo: "Make angular 2 course", completed: true },
+      ],
+    };
+  },
 
-    export defualt {
-        data() {
-            return {
-                todos: [
-                    {todo: "Make angular course", completed :false},
-                    {todo: "Make swelte course", completed :false},
-                    {todo: "Make angular 2 course", completed :true}
-                
-                ]
-            }
-        },
-
-        methods: {
-            addToDo(todo) { this.todos.push(todo) },
-            editToDo(todo, etodo) {todo.todo = etodo},
-            toggleToDo(todo) {todo.completed = !todo.completed},
-            deleteToDo(todo) { this.todos = this.todo.filter(t => t.todo != todo)},
-        }
-    }
-
+  methods: {
+    addToDo(todo) {
+      this.todos.push(todo);
+    },
+    editToDo(todo, etodo) {
+      todo.todo = etodo;
+    },
+    toggleToDo(todo) {
+      todo.completed = !todo.completed;
+    },
+    deleteToDo(todo) {
+      this.todos = this.todo.filter((t) => t.todo != todo);
+    },
+  },
+};
 </script>
 <style>
 body {
-    background-color: rgb(66, 66, 66);
-    color: blue;
+  background-color: rgb(66, 66, 66);
+  color: blue;
 }
-
 </style>
