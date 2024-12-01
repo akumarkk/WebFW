9cb662d52f65ff5cc12f5b92080315d24fb389cd
Anil Kumar K K <akumarkk@outlook.com>
2024-12-01 12:29:18 -0800

    emit event/bubble

---
+++ Vue/apps/displayinput/displayin/src/components/Display.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <DisplayItem @display-text="handleInput">
+
+        </DisplayItem>
+        <p>
+            you entered - {{ message }}!
+        </p>
+    </div>
+    <!-- <input type="text" @input="handleInput"/> -->
+
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import DisplayItem from './DisplayItem.vue';
+
+const message = ref('empty message')
+const handleInput = (value) => {
+    console.log(value);
+    message.value = value;
+    // this.$emit('display-text', event.target.value)
+}
+
+</script>
+<style>
+
+</style>
---
+++ Vue/apps/displayinput/displayin/src/components/DisplayItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <input type="text" @input="handleInput"/>
+
+</template>
+<script setup lang="ts">
+// function handleInput(event) {
+//     console.log(event);
+//     this.$emit('display-text', event.target.value)
+// }
+// const handleInput = (event) => {
+//     console.log(event);
+//     this.$emit('display-text', event.target.value)
+// }
+
+// const emit
+
+const emit = defineEmits(['display-text']);
+const handleInput = (event) => {
+    console.log(event.target.value);
+    emit('display-text', event.target.value)
+}
+</script>
+<style>
+
+</style>
