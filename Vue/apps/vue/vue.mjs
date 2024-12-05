8d1817c8f5e463abaff3fe9cbcc2e1edeb366a07
Anil Kumar K K <akumarkk@outlook.com>
2024-12-04 21:27:27 -0800

    reactivity sys

--- Vue/apps/vue/vue.mjs
+++ Vue/apps/vue/vue.mjs
@@ -3,85 +3,86 @@ const depsMap = new Map();
 let activeEffect = null;
 
 export function watchEffect(effect) {
-  activeEffect = effect;
-  effect();
-  activeEffect = null;
+
+    activeEffect = effect;
+    effect();
+    activeEffect = null;
 }
 
 function track(target, property) {
-  if (activeEffect) {
-    // We get the correct depsMap using the target (reactive object)
-    let depsMap = targetMap.get(target);
-    if (!depsMap) {
-      depsMap = new Map();
-      targetMap.set(target, depsMap);
-    }
+    if (activeEffect) {
+        // We get the correct depsMap using the target (reactive object)
+        let depsMap = targetMap.get(target);
+        if (!depsMap) {
+            depsMap = new Map();
+            targetMap.set(target, depsMap);
+        }
 
-    // Get list of effects of dependency
-    let dep = depsMap.get(property);
-    if (!dep) {
-      dep = new Set();
-      depsMap.set(property, dep);
-    }
+        // Get list of effects of dependency
+        let dep = depsMap.get(property);
+        if (!dep) {
+            dep = new Set();
+            depsMap.set(property, dep);
+        }
 
-    dep.add(activeEffect);
-  }
+        dep.add(activeEffect);
+    }
 }
 
 function trigger(target, property) {
-  // We get the correct depsMap using the target (reactive object)
-  let depsMap = targetMap.get(target);
-  if (!depsMap) return;
+    // We get the correct depsMap using the target (reactive object)
+    let depsMap = targetMap.get(target);
+    if (!depsMap) return;
 
-  // Get list of effects of dependency
-  let dep = depsMap.get(property);
-  if (!dep) return;
+    // Get list of effects of dependency
+    let dep = depsMap.get(property);
+    if (!dep) return;
 
-  // Run all the effects of this dependency
-  dep.forEach((effect) => {
-    effect();
-  });
+    // Run all the effects of this dependency
+    dep.forEach((effect) => {
+        effect();
+    });
 }
 
 export function reactive(targetObject) {
-  return new Proxy(targetObject, {
-    get(target, property) {
-      track(target, property);
-      return target[property];
-    },
+    return new Proxy(targetObject, {
+        get(target, property) {
+            track(target, property);
+            return target[property];
+        },
 
-    set(target, property, value) {
-      let oldValue = target[property];
-      target[property] = value;
+        set(target, property, value) {
+            let oldValue = target[property];
+            target[property] = value;
 
-      // Call update function only if the value changed
-      if (oldValue != value) {
-        trigger(target, property);
-      }
+            // Call update function only if the value changed
+            if (oldValue != value) {
+                trigger(target, property);
+            }
 
-      return true;
-    },
-  });
+            return true;
+        },
+    });
 }
 
 class RefImpl {
-  constructor(value) {
-    this._value = value;
-  }
+    constructor(value) {
+        this._value = value;
+    }
 
-  get value() {
-    track(this, 'value');
-    return this._value;
-  }
+    get value() {
+        track(this, 'value');
+        return this._value;
+    }
 
-  set value(newValue) {
-    this._value = newValue;
-    trigger(this, 'value');
-  }
+    set value(newValue) {
+        this._value = newValue;
+        trigger(this, 'value');
+    }
 }
 
 export function ref(rawValue) {
-  return new RefImpl(rawValue);
+    return new RefImpl(rawValue);
 }
 
 // ref : https://stackblitz.com/edit/vue3-reactivity-watcheffect-reactive-t6dik2?file=ourVue.js
