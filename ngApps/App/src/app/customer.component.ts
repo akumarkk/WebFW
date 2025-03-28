c508a99fc89fffa5ce9111fe93bfac3e00015a64
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-03-27 17:35:10 -0700

    customer comp

--- ngApps/App/src/app/customer.component.ts
+++ ngApps/App/src/app/customer.component.ts
@@ -4,16 +4,23 @@ import {Customer} from './customer.model'
 // import { BrowserModule } from '@angular/platform-browser';
 // import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-root',
     templateUrl: './customer.view.html',
     styleUrls: ['./customer.component.scss'],
-    imports: [FormsModule]
+    imports: [FormsModule, CommonModule]
 })
 export class CustomerComponent{
     title= 'Customer Application';
     CustomerModel : Customer = new Customer();
+    csModels:Array<Customer> = new Array<Customer>();
+
+    Add() {
+        this.csModels.push(this.CustomerModel);
+        this.CustomerModel = new Customer();
+        console.info(this.csModels.length);
+    }
 
     constructor() {
         this.CustomerModel.CustomerAmount = 100;
