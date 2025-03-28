d415b059769c14883b1ed74906266d4ff6acef43
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-03-27 17:13:21 -0700

    customer comp - mv binding

--- ngApps/App/src/app/customer.component.ts
+++ ngApps/App/src/app/customer.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import {Customer} from './customer.model'
 
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     templateUrl: './customer.view.html',
-    styleUrls: ['./customer.component.scss']
+    styleUrls: ['./customer.component.scss'],
+    imports: [FormsModule]
 })
 export class CustomerComponent{
     title= 'Customer Application';
     CustomerModel : Customer = new Customer();
+
+    constructor() {
+        this.CustomerModel.CustomerAmount = 100;
+        this.CustomerModel.CustomerCode = "100CG";
+        this.CustomerModel.CustomerName = "Sudheer";
+    }
     
 }
--- ngApps/App/src/main.server.ts
+++ ngApps/App/src/main.server.ts
@@ -4,6 +4,9 @@ import { config } from './app/app.config.server';
 
 import { CustomerComponent } from './app/customer.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 const bootstrap = () => bootstrapApplication(CustomerComponent, config);
 
 export default bootstrap;
