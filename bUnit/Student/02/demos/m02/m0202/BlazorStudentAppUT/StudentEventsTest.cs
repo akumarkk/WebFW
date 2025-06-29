f34c8953f43e0923b84ced879d097f1fc4293390
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-06-29 13:11:57 -0700

    cut.find,change fill form and assert

--- bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/StudentEventsTest.cs
+++ bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/StudentEventsTest.cs
@@ -43,4 +43,41 @@ public class StudentEventsTest
         Assert.False(cut.Instance.isSuccessMessageVisible);
     }
 
+    [Fact]
+    public void SubmitEvent_ShouldSucceed_NewStudentComponent_Test()
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student() { Name = "Raj", Id = 1, Address = "Pune" },
+            new Student() { Name = "Ram Raj", Id = 2, Address = "Pune" },
+
+        };
+
+        Student student = new Student() { Name = "Yash", Id = 1, Address = "Blr" };
+
+
+        var studentServiceMock = new Mock<IStudentsService>();
+        studentServiceMock.Setup(x => x.GetStudentsAsync()).ReturnsAsync(students);
+        studentServiceMock.Setup(o => o.AddStudentAsync(It.IsAny<Student>())).ReturnsAsync(student);
+
+        var ctx = new TestContext();
+        ctx.Services.AddScoped(p => studentServiceMock.Object);
+
+        // ctx.Services.AddSingleton<IStudentsService>(studentServiceMock.Object);
+
+        var cut = ctx.RenderComponent<BlazorStudentApp.Pages.Student.NewStudent>();
+
+        cut.Find("input[id=\"name\"]").Change("Yash");
+        cut.Find("input[id=\"address\"]").Change("Blr");
+        cut.Find("input[id=\"email\"]").Change("1@craft.com");
+        cut.Find("input[id=\"phone\"]").Change("+91 1234567890");
+        var submitBtn = cut.Find("button[type=\"submit\"]");
+        submitBtn.Click();
+
+        // Assert.Equal(student, cut.Instance.student);
+        // Assert.Equal("Yash", cut.Instance.studentName);
+        Assert.Contains($"""Student Yash added to the database. Fill in the form to add another student.""", cut.Markup);
+        Assert.True(cut.Instance.isSuccessMessageVisible);
+    }
+
 }
