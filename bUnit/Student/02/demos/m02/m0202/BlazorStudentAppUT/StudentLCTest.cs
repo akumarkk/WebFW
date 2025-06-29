b697d3a04150ae41530b47dde5e4080dc3c32aa3
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-06-29 14:55:01 -0700

    exception assert - element find, event hdl not found

--- bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/StudentLCTest.cs
+++ bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/StudentLCTest.cs
@@ -48,7 +48,7 @@ public class StudentLCTest
         var str = "Hello World - BUnit!";
         var ctx = new TestContext();
         var textService = new TaskCompletionSource<string>();
-        
+
         var cut = ctx.RenderComponent<BlazorStudentApp.Pages.Index>(
             p => p.Add(p => p.TextService, textService.Task)
         );
@@ -57,6 +57,85 @@ public class StudentLCTest
         cut.WaitForState(() => cut.Find("h6").TextContent == str);
 
         Assert.Contains($"""<h6>{str}</h6>""", cut.Markup);
+    }
+
+    [Fact]
+    public void Exception__NewStudentComponent_Test()
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student() { Name = "Raj", Id = 1, Address = "Pune" },
+            new Student() { Name = "Ram Raj", Id = 2, Address = "Pune" },
+
+        };
+
+        Student student = new Student() { Name = "Yash", Id = 1, Address = "Blr" };
+
+
+        var studentServiceMock = new Mock<IStudentsService>();
+        studentServiceMock.Setup(x => x.GetStudentsAsync()).ReturnsAsync(students);
+        studentServiceMock.Setup(o => o.AddStudentAsync(It.IsAny<Student>())).ReturnsAsync(student);
+
+        var ctx = new TestContext();
+        ctx.Services.AddScoped(p => studentServiceMock.Object);
+        var cut = ctx.RenderComponent<BlazorStudentApp.Pages.Student.NewStudent>();
+
+        cut.Find("input[id=\"name\"]").Change("Yash");
+        cut.Find("input[id=\"address\"]").Change("Blr");
+        cut.Find("input[id=\"email\"]").Change("1@craft.com");
+        cut.Find("input[id=\"phone\"]").Change("+91 1234567890");
+        // var submitBtn = cut.Find("button[type=\"submitnotfound\"]");
+        // submitBtn.Click();
+
+
+        // Act
+        // Assert
+        Assert.Throws<Bunit.ElementNotFoundException>(() => cut.Find("button[type=\"submitnotfound\"]"));
+
+        // Assert.Equal(student, cut.Instance.student);
+        // Assert.Equal("Yash", cut.Instance.studentName);
+        // Assert.Contains($"""Student Yash added to the database. Fill in the form to add another student.""", cut.Markup);
+        // Assert.True(cut.Instance.isSuccessMessageVisible);
+    }
+    
+    [Fact]
+    public void ExceptionElementNF_NewStudentComponent_Test()
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student() { Name = "Raj", Id = 1, Address = "Pune" },
+            new Student() { Name = "Ram Raj", Id = 2, Address = "Pune" },
+
+        };
+
+        Student student = new Student() { Name = "Yash", Id = 1, Address = "Blr" };
+
+
+        var studentServiceMock = new Mock<IStudentsService>();
+        studentServiceMock.Setup(x => x.GetStudentsAsync()).ReturnsAsync(students);
+        studentServiceMock.Setup(o => o.AddStudentAsync(It.IsAny<Student>())).ReturnsAsync(student);
+
+        var ctx = new TestContext();
+        ctx.Services.AddScoped(p => studentServiceMock.Object);
+        var cut = ctx.RenderComponent<BlazorStudentApp.Pages.Student.NewStudent>();
+
+        cut.Find("input[id=\"name\"]").Change("Yash");
+        cut.Find("input[id=\"address\"]").Change("Blr");
+        cut.Find("input[id=\"email\"]").Change("1@craft.com");
+        cut.Find("input[id=\"phone\"]").Change("+91 1234567890");
+
+        var h1Btn = cut.Find("h1");
+        // submitBtn.Click();
+
+
+        // Act
+        // Assert
+        Assert.Throws<Bunit.MissingEventHandlerException>(() => h1Btn.Click());
+
+        // Assert.Equal(student, cut.Instance.student);
+        // Assert.Equal("Yash", cut.Instance.studentName);
+        // Assert.Contains($"""Student Yash added to the database. Fill in the form to add another student.""", cut.Markup);
+        // Assert.True(cut.Instance.isSuccessMessageVisible);
     }
 
 
