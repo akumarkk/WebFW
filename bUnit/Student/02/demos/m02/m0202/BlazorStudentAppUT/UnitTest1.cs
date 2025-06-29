db0d2bb238fe64c458f627d6e64fe2605ac3335e
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-06-29 01:30:24 -0700

    bUnit test - rendercomp

--- bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/UnitTest1.cs
+++ bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/UnitTest1.cs
@@ -1,10 +1,23 @@
 ﻿namespace BlazorStudentAppUT;
 
+using Bunit;
+using BlazorStudentApp;
+
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Basic_MarkUp_IndexComponentRender()
     {
+        Console.WriteLine("Hello World!");
+
+        // Arrange
+        var ctx = new TestContext();
+
+        // Act
+        var cut = ctx.RenderComponent<BlazorStudentApp.Pages.Index>();
+
+        // Assert
+        cut.MarkupMatches("<h1>Welcome to our school</h1>\n\rThis is the official page to manage students of our School.");
 
     }
 }
