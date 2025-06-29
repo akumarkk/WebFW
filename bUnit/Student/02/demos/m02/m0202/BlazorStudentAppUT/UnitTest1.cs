007b756a0b17f56dc1ad48ca57565fdaa24d4f34
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2025-06-29 02:40:50 -0700

    markupmatches with params

--- bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/UnitTest1.cs
+++ bUnit/Student/02/demos/m02/m0202/BlazorStudentAppUT/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿
 namespace BlazorStudentAppUT;
 
+using BlazorStudentApp.Data.Models;
+
 
 using Bunit;
 using BlazorStudentApp;
@@ -37,6 +39,36 @@ public class UnitTest1
         // Assert
         var h1Ele = cut.Find("h1");
         h1Ele.MarkupMatches("<h1>Welcome to our school</h1>");
+
+    }
+
+
+    [Fact]
+    public void Parameter_Passing_StudentDetailsComponentRender()
+    {
+        Console.WriteLine("Hello World!");
+
+        // Arrange
+        var ctx = new TestContext();
+
+        var heading = "Student Details";
+
+        var student = new Student() { Name = "Raj", Id = 1, Address = "Pune" };
+
+        // Act
+        var cut = ctx.RenderComponent<BlazorStudentApp.Pages.Student.DetailsStudent>(p => p
+            .Add(p => p.Heading, heading)
+            .Add(p => p.SelectedStudent, student));
+
+        Assert.Contains(student.Name, cut.Find(".modal-body h5").TextContent);
+
+        Assert.Contains( @"<h4 class=""modal-title"">" +heading+ "</h4>", cut.Markup);
+        // Assert.Contains(cut.Markup, student.Name);
+        // Assert.Contains(cut.Markup, student.Id.ToString());
+        // Assert.Contains(cut.Markup, student.Address);
+
+        // Assert
+        // cut.MarkupMatches("<h1>Welcome to our school</h1>\n\rThis is the official page to manage students of our School.");
 
     }
 }
